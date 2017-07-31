56f4681ef3109e7323c6a0fe1e3becfc8bc63c55
Sean Nam <sean.nam01@gmail.com>
2017-07-30 18:58:13 -0700

    finished adding personal information and creating functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,186 +1,198 @@
-
-
-var education = [
-    {
-    "schools":
-      {
-        "name": "University of California Riverside",
-        "location": "Riverside, CA",
-        "degree": "Bachelors",
-        "major": "History",
-        "dates": 2016
-      }
-},{
-    "schools":
-      {
-        "name": "Udacity",
-        "location": "Online",
-        "degree": "Front-End Web Development",
-        "dates": 2017
-      }
-},{
-
-    "schools": [
-      {
-        "name": "Santa Monica College",
-        "city": "Santa Monica, CA",
-        "course": "Computer Science 3: Intro to Computer Systems",
-        "date": ["February 2016", "June 2016"]
-      },
-      {
-        "name": "Santa Monica College",
-        "city": "Santa Monica, CA",
-        "course": "Computer Science 80: Internet Programming",
-        "date": ["February 2016", "June 2016"]
-      }
-    ]
-}]
-
 var bio = {
 
     "name": "Sean Nam",
     "role": "Junior Developer",
-    "number": "(222)333-5555",
-    "email": "seannam@email.com",
-    "github": "seannam@github.com",
-    "pic": "images/fry.jpg",
-    "twitter": "seannam@twitter",
-    "location": "Los Angeles",
-    "skills": ["HTML", "CSS", "JavaScript", "jQuery"]
-
-}
-
-var work = [
-    {
-    "position": "Sales Representative",
-    "employer": "TM Wireless",
-    "dates": "July 2016 - March 2017",
-    "location": "Los Angeles",
-    "description": "Sales Job"
+    "contacts": {
+        "mobile": "(222)333-5555",
+        "email": "seannam@email.com",
+        "github": "seannam@github.com",
+        "location": "Los Angeles"
     },
-    {
-    "position": "Computer Technician",
-    "employer": "Westlake Park Square",
-    "dates": "May 2015 - February 2016",
-    "location": "Los Angeles",
-    "description": "Computer Work"
-    }
-]
+    "welcomeMessage": "Welcome to my Online Resume",
+    "skills": ["HTML", "CSS", "Bootstrap", "Grunt", "npm", "JavaScript", "jQuery", "GitHub", "Microsoft Office"],
+    "biopic": "images/fry.jpg"
+};
 
-var projects = [
-    {
-    "title": "Shopping Cart",
-    "date": "2016",
-    "description": "This project was from my CS 80 course. It was a team project where we created a website for a mock t-shirt company. My main contribution was the shopping cart system. I used JavaScript and jQuery to create the ability to update the total quantity and cost of what was input into the shopping cart",
-    "image": "images/shoppingcart2.jpg"
-    },
-    {
-    "title": "Trading Card",
-    "date": 2017,
-    "description": "A CSS project designing a trading card.",
-    "image": "images/tradingcard2.jpg"
-    }
-]
 
+
+
+var education = {
+
+    "schools": [{
+        "name": "University of California Riverside",
+        "location": "Riverside, CA",
+        "degree": "Bachelors",
+        "majors": "History",
+        "dates": 2016,
+        "url": "http://www.ucr.edu/"
+    }],
+    "onlineCourses": [{
+            "title": "Front-End Web Developer Nanodegree Program",
+            "school": "Udacity",
+            "dates": "April 2017 - August 2017",
+            "url": "www.udacity.com"
+        },
+        {
+            "title": "Computer Science 3: Intro to Computer Systems",
+            "school": "Santa Monica College",
+            "dates": "February 2016",
+            "url": "http://smc.edu/"
+        },
+        {
+            "title": "Computer Science 80: Internet Programming",
+            "school": "Santa Monica College",
+            "dates": "June 2016",
+            "url": "http://smc.edu/"
+        }
+    ]
+};
+
+
+
+var work = {
+
+    "jobs": [{
+            "employer": "TM Wireless",
+            "title": "Sales Representative",
+            "location": "Los Angeles",
+            "dates": "July 2016 - March 2017",
+            "description": "Sales Job"
+        },
+        {
+            "employer": "Westlake Park Square",
+            "title": "Computer Technician",
+            "location": "Los Angeles",
+            "dates": "May 2015 - February 2016",
+            "description": "Computer Technician"
+        }
+    ]
+};
+
+
+
+var projects = {
+
+    "projects": [{
+            "title": "Shopping Cart",
+            "dates": "2016",
+            "description": "This project was from my CS 80 course. It was a team project where we created a website for a mock t-shirt company. My main contribution was the shopping cart system. I used JavaScript and jQuery to create the ability to update the total quantity and cost of what was input into the shopping cart",
+            "images": "images/shoppingcart2.jpg"
+        },
+        {
+            "title": "Trading Card",
+            "dates": "2017",
+            "description": "A CSS project designing a trading card.",
+            "images": "images/tradingcard2.jpg"
+        }
+    ]
+};
 
 
 $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
 $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-
-
-$("#topContacts").prepend(HTMLmobile.replace("%data%", bio.number));
-$("#topContacts").append(HTMLemail.replace("%data%", bio.email));
-$("#topContacts").append(HTMLgithub.replace("%data%", bio.github));
-$("#topContacts").append(HTMLtwitter.replace("%data%", bio.twitter));
-$("#topContacts").append(HTMLlocation.replace("%data%", bio.location));
-
-$("#header").append(HTMLbioPic.replace("%data%", bio.pic));
-
+$("#topContacts").prepend(HTMLmobile.replace("%data%", bio.contacts.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
 
 /------ Bio + display ------/
 
 bio.display = function() {
-    if(bio.skills.length > 0) {
+    if (bio.skills.length > 0) {
 
         $("#header").append(HTMLskillsStart);
+        var formattedSkill;
 
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-        $("#skills").append(formattedSkill);
-        formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-        $("#skills").append(formattedSkill);
-        formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-        $("#skills").append(formattedSkill);
-        formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-        $("#skills").append(formattedSkill);
+        for (skill in bio.skills) {
+            formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+};
 
-}
-}
 bio.display();
 
+/------ Education + display ------/
 
-/------ Displays Work Section ------/
+education.display = function() {
+
+    $("#education").append(HTMLschoolStart);
+
+    for (a in education.schools) {
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[a].name);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[a].degree);
+        var formattedSchoolNameDegree = formattedSchoolName + formattedSchoolDegree;
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[a].dates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[a].location);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[a].majors);
+
+        $(".education-entry:last").append(formattedSchoolNameDegree);
+        $(".education-entry:last").append(formattedSchoolDates);
+        $(".education-entry:last").append(formattedSchoolLocation);
+        $(".education-entry:last").append(formattedSchoolMajor);
+    }
+
+    $("#education").append(HTMLonlineClasses);
+    $("#education").append(HTMLschoolStart);
+
+    for (a in education.onlineCourses) {
+
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[a].title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[a].school);
+        var formattedOnlineTitleSchool = formattedOnlineTitle + formattedOnlineSchool;
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[a].dates);
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[a].url);
+
+        $(".education-entry:last").append(formattedOnlineTitleSchool);
+        $(".education-entry:last").append(formattedOnlineDates);
+        $(".education-entry:last").append(formattedOnlineURL);
+    }
+};
+
+education.display();
+
+
+/------ Work + display ------/
 
 function displayWork() {
 
-    for(job in work) {
+    $("#workExperience").append(HTMLworkStart);
 
-        /------ Work Experience Header ------/
+    for (job in work.jobs) {
 
-        $("#workExperience").append(HTMLworkStart);
-
-        /------ Employer-Position------/
-
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work[job].employer);
-        var formattedPosition = HTMLworkTitle.replace("%data%", work[job].position);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedPosition = HTMLworkTitle.replace("%data%", work.jobs[job].title);
         var formattedEmployerPosition = formattedEmployer + formattedPosition;
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+
         $(".work-entry:last").append(formattedEmployerPosition);
-
-        /------ Work Dates ------/
-
-        var formattedDates = HTMLworkDates.replace("%data%", work[job].dates);
         $(".work-entry:last").append(formattedDates);
-
-        var formattedDescription = HTMLworkDescription.replace("%data%", work[job].description);
         $(".work-entry:last").append(formattedDescription);
-
-
     }
 }
 
 displayWork();
 
 
-/------ Click function ------/
-
-
-$(document).click(function(loc) {
-
-    var x = loc.pageX
-    var y = loc.pageY
-
-    logClicks(x, y);
-
-});
-
-
 /------ Internationalize Button -------/
-
 
 $("#main").append(internationalizeButton);
 
-
-
 function inName(x) {
 
-  var first = "";
-  var last = "";
-  var fullName = "";
+    var first = "";
+    var last = "";
+    var fullName = "";
 
-  first = x[0].toUpperCase() + x.slice(1, x.indexOf(" ")).toLowerCase();
-  last = x.slice(x.indexOf(" ")).toUpperCase();
-  fullName = first + " " + last;
+    first = x[0].toUpperCase() + x.slice(1, x.indexOf(" ")).toLowerCase();
+    last = x.slice(x.indexOf(" ")).toUpperCase();
+    fullName = first + " " + last;
 
-  return fullName;
+    return fullName;
 
 }
 
@@ -191,59 +203,36 @@ $(document).ready(function() {
         /--- HTMLheaderName has <h1 id="name"> ---/
 
         var $name = $('#name');
-        var iName = inName($name.text()) || function(){};
+        var iName = inName($name.text()) || function() {};
         $name.html(iName);
     });
 });
 
-/------ Projects with Display ------/
+
+/------ Projects + display ------/
 
 
 projects.display = function() {
-    for (x in projects) {
-        $("#projects").append(HTMLprojectStart);
 
-        var formattedProject = HTMLprojectTitle.replace("%data%", projects[x].title);
+    $("#projects").append(HTMLprojectStart);
+
+    for (x in projects.projects) {
+
+        var formattedProject = HTMLprojectTitle.replace("%data%", projects.projects[x].title);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[x].dates);
+        var formattedDates = HTMLprojectDescription.replace("%data%", projects.projects[x].description);
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[x].images);
+
         $(".project-entry:last").append(formattedProject);
-
-        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects[x].date);
         $(".project-entry:last").append(formattedProjectDates);
-
-        var formattedDates = HTMLprojectDescription.replace("%data%", projects[x].description);
         $(".project-entry:last").append(formattedDates);
-
-        var formattedImage = HTMLprojectImage.replace("%data%", projects[x].image);
         $(".project-entry:last").append(formattedImage);
-
- }}
+    }
+}
 
 projects.display();
 
-/------ Education ------/
 
-education.display = function() {
-    for(a in education){
-        $("#education").append(HTMLschoolStart);
+/-------- Map Div ------/
 
-        var formattedSchoolName = HTMLschoolName.replace("%data%", education[a].schools.name);
-        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education[a].schools.degree);
-        var formattedSchoolNameDegree = formattedSchoolName + formattedSchoolDegree;
-
-        $(".education-entry:last").append(formattedSchoolNameDegree);
-
-        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education[a].schools.dates);
-        $(".education-entry:last").append(formattedSchoolDates);
-
-        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education[a].schools.location);
-        $(".education-entry:last").append(formattedSchoolLocation);
-
-        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education[a].schools.major);
-        $(".education-entry:last").append(formattedSchoolMajor);
-}
-}
-
-education.display();
-
-
-
-/------ Google Map ------/
+$("#mapDiv").append(googleMap);
